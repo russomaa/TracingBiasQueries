bdf732571f9d731fde47336d8bdcadf6b86d19d6
sawischa <sawischa@gmail.com>
2023-02-09 09:42:18 +0100

    Sample mined log

--- rule-mining/start_rule_mining.sh
+++ rule-mining/start_rule_mining.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-FILES="./../../../data/services/GraphDB/data/graphdb-import/BenchmarkLPDatasets/*"
+FILES="./../BenchmarkLPDatasets/*"
 for file_path in $FILES
 do
   filename=$(basename -- "$file_path")
   extension="${filename##*.}"
   filename="${filename%.*}"
   echo "Mining rules from $file_path"
-  java -jar amie/amie-dev.jar $file_path -d" " > ./../mined_rules/raw_logs/$filename.txt  
+  mkdir mined_rules
+  mkdir mined_rules/raw_logs
+  java -jar amie-dev.jar $file_path -d" " > mined_rules/raw_logs/$filename.txt
 done
 
